a42a4e8ae5ab3899380b49d8cd22fbc1d9bbc5e3
Solomon Eseme <kaperskyguru@gmail.com>
2024-03-11 15:57:38 +0100

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,22 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
 
   // ssr: false,
+  app: {
+    head: {
+      title: "Online Code Playground",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "The user friendly code playground and online compiler for server-side programming langauges and backend engineers that allows you to Write code in any backend programming languages and run it online. The code playground and text editor also supports taking input from the user and standard libraries. It uses any of bcakend programming language's compiler to compile code.",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
 
   components: {
     dirs: [
