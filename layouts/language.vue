9729784e8906fb05576bdbf87862bedadd55e372
Solomon Eseme <kaperskyguru@gmail.com>
2025-02-26 06:02:14 +0100

    feat: add python 30 ad

--- layouts/language.vue
+++ layouts/language.vue
@@ -1,5 +1,23 @@
 <template>
   <main>
+    <div
+      class="bg-yellow-400 sticky top-0 w-full"
+      style="padding: 10px; display: flex; justify-content: center"
+    >
+      <h2
+        class="flex items-center gap-1.5 text-xs font-medium text-black md:text-base"
+        style="font-size: 1rem; font-weight: bold"
+      >
+        Unlock Your Python Backend Career: Build 30 Projects in 30 Days.
+
+        <a
+          class="items-center gap-1.5 rounded-full bg-black px-2 py-0.5 text-sm text-xs font-medium uppercase tracking-wide text-white hover:bg-zinc-800 sm:px-3 sm:py-1"
+          target="_blank"
+          href="https://python30.masteringbackend.com?ref=playgrounds"
+          >Join now for just $54</a
+        >
+      </h2>
+    </div>
     <slot />
   </main>
 </template>
